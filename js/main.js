17d5fab7207ab3b5e8459915816147cd32bf15f2
sahbia <ouaja.sahbia@gmail.com>
2017-07-19 16:35:09 +0200

    add base structure html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,48 @@
-$('#applaction').css('width','400').css('heigth','600').css('backgroudColor','red');
+$('#applaction').css({
+            
+            "background-color": "rgb(17,146,255)",
+            "font-family": "Josefin Sans",
+            "font-size": "20px",
+             "width" :"500px",
+            "height":"450px",
+            "margin":"auto",
+             "border":"1px solid transparent",
+            "border-radius": "3px"
+        });
+
+$('#pseud').css({
+            
+            "background-color": "rgb(17,146,255)",
+            "font-family": "Josefin Sans",
+            "font-size": "20px",
+            "padding":"5px",
+            "width" :"500px",
+            "height":"50px",
+            "margin":"auto",
+            "border-top":"3px solidrgb(230,230,230)",
+            "border-radius": "3px"
+        });
+
+
+$('#bienvenu').css({  
+               "width" :"500px",
+               "height":"50px",
+               "border-bottom":"3px solid rgb(230,230,230)" 
+              
+	});
+$('#chat').css({  
+               "width" :"500px",
+               "height":"355px",
+               "overflow":"scroll"
+               //"border-top":"1px solid rgb(155,155,155)"  
+	});
+
+
+$('#message').css({  
+               
+                 "height":"45px",
+                 "border-top":"1px solid rgb(155,155,155)",
+               
+	         });
+               
+	//    
